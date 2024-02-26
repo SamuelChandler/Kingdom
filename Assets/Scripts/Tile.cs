dd05e75792048aeb7a8e56c58c9e0c961445fb75
BlitzMK <sec180005@utdallas.edu>
2024-02-25 23:49:57 -0600

    tile highlighting

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -6,9 +6,22 @@ public class Tile : MonoBehaviour
 {
     [SerializeField] private Color _baseColor, _offsetColor;
     [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private GameObject _highlight;
 
     public void Init(bool isOffset)
     {
         _renderer.color = isOffset ? _offsetColor : _baseColor;
+        _highlight.SetActive(false);
     }
+
+    private void OnMouseEnter()
+    {
+        _highlight.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    { 
+        _highlight.SetActive(false); 
+    }
+
 }

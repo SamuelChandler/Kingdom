6c310df7f78aff4ef5a66268a472db7d3bea3084
BlitzMK <sec180005@utdallas.edu>
2024-02-26 20:59:27 -0600

    mountain and grass tiles

--- Assets/Scripts/Board_Manager.cs
+++ Assets/Scripts/Board_Manager.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
+//using System;
 
 public class Board_Manager : MonoBehaviour
 {
     [SerializeField] private int _width, _height;
 
-    [SerializeField] private Tile _tile_Prefab;
+    [SerializeField] private Tile _grassTile,_mountainTile;
 
     [SerializeField] private Transform _camera;
 
@@ -23,7 +23,8 @@ public class Board_Manager : MonoBehaviour
         _tiles = new Dictionary<Vector2, Tile>();
         for(int i = 0; i < _width; i++){
             for(int j = 0; j < _height; j++){
-                var spawnedTile = Instantiate(_tile_Prefab, new Vector3(i, j), Quaternion.identity);
+                var randTile = Random.Range(0,6) == 3 ? _mountainTile : _grassTile;
+                var spawnedTile = Instantiate(randTile, new Vector3(i, j), Quaternion.identity);
                 spawnedTile.name = $"Tile {i} {j}";
 
                 var isOffset = (i % 2 == 0 && j % 2 != 0) || (i % 2 != 0 && j % 2 == 0);

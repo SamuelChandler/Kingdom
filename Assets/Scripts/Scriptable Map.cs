56321c91dbf83e7ad286f11a0ec653ef64d44a40
BlitzMK <sec180005@utdallas.edu>
2024-03-29 12:54:45 -0500

    new map can now be gennerated based on scriptable objects

--- Assets/Scripts/Scriptable Map.cs
+++ Assets/Scripts/Scriptable Map.cs
@@ -10,12 +10,34 @@ public class ScriptableMap : ScriptableObject
     [SerializeField] public int width;
 
     [SerializeField] [Multiline] public String MapTiles = "";
-    
+
+    [SerializeField] public Tile emptyTile;
     [SerializeField] public Tile t1;
     [SerializeField] public Tile t2;
     [SerializeField] public Tile t3;
     [SerializeField] public Tile t4;
     [SerializeField] public Tile t5;
     
+    public Tile GetTile(char id)
+    {
+        switch (id)
+        {
+            case '0':
+                return emptyTile;
+            case '1':
+                return t1;
+            case '2':
+                return t2;
+            case '3':
+                return t3;
+            case '4':
+                return t4;
+            case '5':
+                return t5;
+            default:
+                return null;
+        }
+    }
+
 }
 

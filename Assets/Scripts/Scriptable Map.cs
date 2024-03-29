5b82e72988ac669a89046d34ae635c5c5a6ab19a
BlitzMK <sec180005@utdallas.edu>
2024-03-29 12:06:17 -0500

    scriptable map created

--- Assets/Scripts/Scriptable Map.cs
+++ Assets/Scripts/Scriptable Map.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,10 +6,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Map", menuName = "Scriptable Map")]
 public class ScriptableMap : ScriptableObject
 {
-    [SerializeField] public int[,] map_r0;
+    [SerializeField] public int height;
+    [SerializeField] public int width;
+
+    [SerializeField] [Multiline] public String MapTiles = "";
     
-
-    [SerializeField]
-    public Tile[] tiles;
-
+    [SerializeField] public Tile t1;
+    [SerializeField] public Tile t2;
+    [SerializeField] public Tile t3;
+    [SerializeField] public Tile t4;
+    [SerializeField] public Tile t5;
+    
 }
+

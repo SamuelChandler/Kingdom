5c36d67b715fe8d9f5c3b396d30ddeb97d9906a3
BlitzMK <sec180005@utdallas.edu>
2024-04-07 19:39:47 -0500

    base player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    private Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //get user movment input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + (movement*moveSpeed*Time.fixedDeltaTime));
     }
 }

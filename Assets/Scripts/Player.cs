4c179bd62dc44bb317a0db82f195ae5c3c11eea4
BlitzMK <sec180005@utdallas.edu>
2024-04-08 22:12:39 -0500

    added cinemachine and camera follow in overworld

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,12 +19,13 @@ public class Player : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+        rb.MovePosition(rb.position + (movement * moveSpeed * Time.fixedDeltaTime));
 
     }
 
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + (movement*moveSpeed*Time.fixedDeltaTime));
+        
     }
 }

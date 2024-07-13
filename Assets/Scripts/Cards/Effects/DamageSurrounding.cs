cbf2ec90a0d3259e6f51bd6db02589ac4c1303ac
Samuel Chandler <sec180005@utdallas.edu>
2024-07-12 21:43:36 -0500

    finishing up for Settings and unit changes

--- Assets/Scripts/Cards/Effects/DamageSurrounding.cs
+++ Assets/Scripts/Cards/Effects/DamageSurrounding.cs
@@ -12,7 +12,43 @@ public class DamageSurrounding: Effect{
 
 
     public override void ActivateEffect(Structure s){
-        //does nothing for now
+        if(s._structure.Faction == Faction.Hero){
+
+            //for each enemy, check distance and apply damage accourdingly
+            foreach(BaseEnemy e in Board_Manager.instance._enemies){
+                bool withinOne = Board_Manager.instance.WithinOne(e.OccupiedTile,s.OccupiedTiles[0,0]);
+                if(withinOne){
+                    e.TakeDamage(_damageAmount);
+                }
+            }
+
+            //for each enemy structure, check distance and apply damage accourdingly
+            foreach(EnemyStructure e in Board_Manager.instance._EnemyStructures){
+                 bool withinOne = Board_Manager.instance.WithinOne(e.OccupiedTiles[0,0],s.OccupiedTiles[0,0]);
+                if(withinOne){
+                    e.TakeDamage(_damageAmount);
+                }
+            }
+
+
+        }else if(s._structure.Faction == Faction.Enemy){
+
+            //for each enemy, check distance and apply damage accourdingly
+            foreach(BaseHero h in Board_Manager.instance._heroes){
+                bool withinOne = Board_Manager.instance.WithinOne(h.OccupiedTile,s.OccupiedTiles[0,0]);
+                if(withinOne){
+                    h.TakeDamage(_damageAmount);
+                }
+            }
+
+            //for each enemy structure, check distance and apply damage accourdingly
+            foreach(AllyStructure h in Board_Manager.instance._AllyStructures){
+                bool withinOne = Board_Manager.instance.WithinOne(h.OccupiedTiles[0,0],s.OccupiedTiles[0,0]);
+                if(withinOne){
+                    h.TakeDamage(_damageAmount);
+                }
+            }
+        }
     }
 
     public override void ActivateEffect(BaseUnit u){
